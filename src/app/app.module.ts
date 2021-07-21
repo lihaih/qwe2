ba325a69ec96c5fc9880242d192e9f6c1919bf45
qwe <123456>
2021-07-21 14:57:05 +0800

    请问去翁

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,30 @@
+/*
+ * @Author: your name
+ * @Date: 2021-07-20 10:38:36
+ * @LastEditTime: 2021-07-20 11:28:25
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \my-angular\src\app\app.module.ts
+ */
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// import { NgModel } from '@angular/forms';
+
+//双向数据绑定
+// import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AbentComponent } from './components/abent/abent.component';
+import { AbterComponent } from './components/abter/abter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AbentComponent,
+    AbterComponent
   ],
   imports: [
     BrowserModule,
